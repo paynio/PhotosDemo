38e67c9704c2891d19babc09221bb14b22a6a1d6
Dan Payne <dan@cyanfive.com>
2016-12-27 10:25:14 +0000

    Project setup (#2)
    * Initial Commit
    
    * Original project commit

---
+++ PhotosDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotosDemo
+//
+//  Created by Dan on 27/12/2016.
+//  Copyright © 2016 Daniel Payne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
